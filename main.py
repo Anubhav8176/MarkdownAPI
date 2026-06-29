c92fa8d7c450e8d7309dde1e0b1d1647afc3ce25
Anubhav Singh <86763919+Anubhav8176@users.noreply.github.com>
2026-06-29 22:59:13 +0530

    feat: html rendered notes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from uuid import UUID
+import markdown
 from fastapi import FastAPI, UploadFile, File, HTTPException, Depends
 from database.database import get_db, Base, db_engine
+from models.NoteData import MarkdownNote
 
 app = FastAPI()
 
@@ -17,12 +20,57 @@ def validate_file(file: UploadFile):
         raise HTTPException(400, f"Invalid content type: {file.content_type}")
 
 
-@app.post("/upload/")
+@app.post("/save")
 async def upload_file(file: UploadFile = File(...), db = Depends(get_db)):
     validate_file(file)
 
     contents = await file.read()
+    file_name = file.filename
+
+    new_note = MarkdownNote(file_name = file_name, content = contents)
+     
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
 
     return {
-        "content" : contents.decode("utf-8")
+        "content" : contents.decode("utf-8"),
+        "file_name": file_name
     }
+
+
+@app.get("/all_notes")
+def get_all_notes(db = Depends(get_db)):
+	notes = db.query(MarkdownNote).all()
+	return notes
+
+
+@app.get("/note/{id}")
+def get_note(id: str, db = Depends(get_db)):
+
+	try:
+		uuid_id = UUID(id)
+	except ValueError:
+		raise HTTPException(status_code=400, detail="Invalid id format")
+	
+	note = db.query(MarkdownNote).filter(MarkdownNote.id == uuid_id).first()
+	return note
+
+
+@app.get("/get_markdown/{id}")
+def get_markdown(id: str, db = Depends(get_db)):
+	try:
+		uuid_id = UUID(id)
+	except ValueError:
+		raise HTTPException(status_code=400, detail="Invalid id format!!")
+
+	note = db.query(MarkdownNote).filter(MarkdownNote.id == uuid_id).first()
+
+	html_markdown_note = markdown.markdown(note.content)
+
+	return html_markdown_note
+
+
+@app.post("/check_grammer")
+async def check_grammer(file: UploadFile = File(...)):
+	validate_file(file=file)
